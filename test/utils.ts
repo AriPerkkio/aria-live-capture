44b56cdd977e7f10e92b97f1a169e22083aa48a1
Ari Perkkiö <ari.perkkio@gmail.com>
2021-09-23 15:33:33 +0000

    test: refactor describe blocks

--- test/utils.ts
+++ test/utils.ts
@@ -2,18 +2,23 @@ interface TestAttributes {
     name?: 'role' | 'aria-live';
     value?: 'status' | 'alert' | 'log' | 'assertive' | 'polite';
     tag?: 'div' | 'output';
+    testName: string;
 }
 
 export const POLITE_CASES: TestAttributes[] = [
-    { name: 'role', value: 'status' },
-    { name: 'role', value: 'log' },
-    { name: 'aria-live', value: 'polite' },
-    { tag: 'output' },
+    { name: 'role', value: 'status', testName: '[role="status"]' },
+    { name: 'role', value: 'log', testName: '[role="log"]' },
+    { name: 'aria-live', value: 'polite', testName: '[aria-live="polite"]' },
+    { tag: 'output', testName: '<output>' },
 ];
 
 export const ASSERTIVE_CASES: TestAttributes[] = [
-    { name: 'role', value: 'alert' },
-    { name: 'aria-live', value: 'assertive' },
+    { name: 'role', value: 'alert', testName: '[role="alert"]' },
+    {
+        name: 'aria-live',
+        value: 'assertive',
+        testName: '[aria-live="assertive"]',
+    },
 ];
 
 export function appendToRoot(element: HTMLElement): void {
