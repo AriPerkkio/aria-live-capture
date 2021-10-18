7326ba5303e602cec962f58c1e61f8d4564fd128
Ari Perkkio <ari.perkkio@gmail.com>
2021-10-18 17:50:49 +0300

    docs: add more role="status" stories

--- .storybook/stories/RoleStatus.stories.ts
+++ .storybook/stories/RoleStatus.stories.ts
@@ -32,3 +32,97 @@ export function LiveRegionUnavailableBeforeContent() {
         `
     );
 }
+
+addStoryName(AnchestorIsHidden, 'FAIL');
+export function AnchestorIsHidden() {
+    return createMountToggle(
+        `
+        <div aria-hidden="true">
+            <div role="status">
+            </div>
+        </div>
+        `,
+        `
+        <div aria-hidden="true">
+            <div role="status">
+                Hello world
+            </div>
+        </div>
+        `
+    );
+}
+
+addStoryName(LiveContainerIsHidden, 'FAIL');
+export function LiveContainerIsHidden() {
+    return createMountToggle(
+        `
+        <div aria-hidden="true" role="status">
+        </div>
+        `,
+        `
+        <div aria-hidden="true" role="status">
+            Hello world
+        </div>
+        `
+    );
+}
+
+addStoryName(ContentIsHidden, 'FAIL');
+export function ContentIsHidden() {
+    return createMountToggle(
+        `
+        <div role="status">
+        </div>
+        `,
+        `
+        <div role="status">
+            <div aria-hidden="true">
+                Hello world
+            </div>
+        </div>
+        `
+    );
+}
+
+addStoryName(ContentIsPartiallyHidden, 'PARTIAL');
+export function ContentIsPartiallyHidden() {
+    return createMountToggle(
+        `
+        <div role="status">
+        </div>
+        `,
+        `
+        <div role="status">
+            <span>
+                Hello
+            </span>
+            <span aria-hidden="true">
+                world
+            </span>
+        </div>
+        `
+    );
+}
+
+addStoryName(PartOfContentChanges, 'PASS');
+export function PartOfContentChanges() {
+    return createMountToggle(
+        `
+        <div role="status">
+            <span>
+                Hello
+            </span>
+        </div>
+        `,
+        `
+        <div role="status">
+            <span>
+                Hello
+            </span>
+            <span>
+                world
+            </span>
+        </div>
+        `
+    );
+}
