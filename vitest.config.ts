d2cb6b92f443d5072ed9c9819b396c3ad469e88b
Ari Perkkiö <ari.perkkio@gmail.com>
2024-04-26 17:26:08 +0300

    test: add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,16 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         reporters: process.env.CI ? 'default' : 'verbose',
+        coverage: {
+            enabled: true,
+            provider: 'istanbul',
+            include: ['src'],
+            thresholds: {
+                statements: 90,
+                branches: 85,
+                functions: 95,
+                lines: 90,
+            },
+        },
     },
 });
