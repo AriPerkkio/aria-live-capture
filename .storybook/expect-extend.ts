140c0f73afdf0ab0d9264600395d9ad95ca7ab76
Ari Perkkiö <ari.perkkio@gmail.com>
2026-09-03 15:29:34 +0300

    chore(dev-deps): vitest 5

--- .storybook/expect-extend.ts
+++ .storybook/expect-extend.ts
@@ -1,5 +1,7 @@
+import "vitest";
+
 declare module "vitest" {
-  interface Assertion<T = any> {
+  interface Matchers<R, T> {
     /**
      * Assert whether given message was announced by ARIA live region.
      *
@@ -7,16 +9,4 @@ declare module "vitest" {
      */
     toBeAnnounced(politenessSetting?: "assertive" | "polite"): T;
   }
-  // Vitest requires asymmetric matchers to be defined separately
-  // for use with expect.stringContaining(), expect.arrayContaining(), etc.
-  interface AsymmetricMatchersContaining {
-    /**
-     * Assert whether given message was announced by ARIA live region.
-     *
-     * @param politenessSetting `POLITENESS_SETTING` of the announcement
-     */
-    toBeAnnounced(politenessSetting?: "assertive" | "polite"): any;
-  }
 }
-
-export {};
